707756f085bc4b5cdbba14dfddde84577a5aa3bd
Urmish Thakker <uthakker@sally.cs.wisc.edu>
2015-10-10 18:20:49 -0500

    Second Commit

---
+++ MICEMAZE/solution.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int NcellsInMaze;
+        int exitCell;
+	int numConnections;
+	scanf("%d",&NcellsInMaze);
+	scanf("%d",&exitCell);
+	scanf("%d",&numConnections);
+	while (numConnections > 0)
+	{
+	      numConnections--;
+	}
+}
+
+
+
+
